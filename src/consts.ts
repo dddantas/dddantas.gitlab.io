02fc0548242a7a33f99caa77febd583c41475ce6
Gustavo Dantas <gusdantas15@gmail.com>
2024-11-23 19:50:03 -0300

    feat: cleans some stuff and deploys using github pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE = {
+  title: "dantas'place ",
+  website: 'https://dddantas.github.io',
+  description: 'Public writings',
+  author: 'Gustavo Dantas <gusdantas15@gmail.com>',
+};
